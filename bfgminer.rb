8e57c71711d92a64cd67e302fba69bb90dad94b6
Vincent Gariepy <caktux@gmail.com>
2013-07-10 19:10:39 -0400

    Switch bfgminer to git only

--- bfgminer.rb
+++ bfgminer.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Bfgminer < Formula
     homepage 'https://github.com/luke-jr/bfgminer'
-    url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/3.1.2/bfgminer-3.1.2.tbz2'
-    sha1 '6c1b764f0123cafbbc72061ac9f21ae527c36223'
+    head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
+    url 'https://github.com/luke-jr/bfgminer.git', :tag => 'bfgminer-3.1.2'
+    version '3.1.2'
     
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
@@ -14,6 +15,7 @@ class Bfgminer < Formula
     depends_on 'jansson'
     
     def install
+        system "./autogen.sh"
         system "./configure", "--disable-debug", "--disable-dependency-tracking",
         "--prefix=#{prefix}",
         "PKG_CONFIG_PATH=/usr/local/opt/curl/lib/pkgconfig:/usr/local/opt/jansson/lib/pkgconfig",
@@ -22,7 +24,9 @@ class Bfgminer < Formula
     end
 
     def patches
-      "https://github.com/luke-jr/bfgminer/commit/4174a1d0589bb94c65e186d0c10142ba8f2176ae.patch"
+      unless build.head?
+        "https://github.com/luke-jr/bfgminer/commit/4174a1d0589bb94c65e186d0c10142ba8f2176ae.patch"
+      end
     end
 
     test do
