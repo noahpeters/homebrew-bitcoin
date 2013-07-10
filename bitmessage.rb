c5f58f9b4883a2e9617a7cbca377de6409fd3dca
Vincent Gariepy <caktux@gmail.com>
2013-07-10 19:48:59 -0400

    Don't link bitmessage app, add caveats telling to 'brew linkapps'

--- bitmessage.rb
+++ bitmessage.rb
@@ -3,7 +3,8 @@ require 'formula'
 class Bitmessage < Formula
     
   homepage 'https://github.com/Bitmessage/PyBitmessage'
-  head 'https://github.com/Bitmessage/PyBitmessage.git'
+  url 'https://github.com/Bitmessage/PyBitmessage.git', :branch => 'master'
+  version 'HEAD'
 
   depends_on 'python'
   depends_on 'pyqt'
@@ -12,6 +13,10 @@ class Bitmessage < Formula
   def install
     system "./osx.sh HEAD"
     bin.install "src/dist/Bitmessage.app"
-    ln_s "#{prefix}/bin/Bitmessage.app", "/Applications/Bitmessage.app"
+  end
+
+  def caveats; <<-EOS.undent
+    You will need to run 'brew linkapps' to link the Bitmessage app.
+    EOS
   end
 end
