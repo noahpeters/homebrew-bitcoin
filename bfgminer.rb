0f301107178785f7820460a5ef6538ef8ab57fda
Vincent Gariepy <caktux@gmail.com>
2013-07-14 11:56:21 -0400

    Update bfgminer to 3.1.3

--- bfgminer.rb
+++ bfgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Bfgminer < Formula
     homepage 'https://github.com/luke-jr/bfgminer'
     head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
-    url 'https://github.com/luke-jr/bfgminer.git', :tag => 'bfgminer-3.1.2'
-    version '3.1.2'
+    url 'https://github.com/luke-jr/bfgminer.git', :tag => 'bfgminer-3.1.3'
+    version '3.1.3'
     
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
@@ -21,12 +21,6 @@ class Bfgminer < Formula
         "PKG_CONFIG_PATH=/usr/local/opt/curl/lib/pkgconfig:/usr/local/opt/jansson/lib/pkgconfig",
         "--enable-scrypt"
         system "make", "install"
-    end
-
-    def patches
-      unless build.head?
-        "https://github.com/luke-jr/bfgminer/commit/4174a1d0589bb94c65e186d0c10142ba8f2176ae.patch"
-      end
     end
 
     test do
