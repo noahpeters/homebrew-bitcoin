c6b58dc224bf334fa8deec6380c309f592a35454
Vincent Gariepy <caktux@gmail.com>
2013-09-26 15:53:29 -0400

    Update cgminer to 3.4.3 and bfgminer to 3.2.1

--- cgminer.rb
+++ cgminer.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Cgminer < Formula
     homepage 'https://github.com/ckolivas/cgminer'
-    url 'https://github.com/ckolivas/cgminer.git', :tag => 'v3.3.4'
-    version '3.3.4'
-    
+    url 'https://github.com/ckolivas/cgminer.git', :tag => 'v3.4.3'
+    version '3.4.3'
+
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
     depends_on 'libtool' => :build
@@ -13,9 +13,11 @@ class Cgminer < Formula
     depends_on 'curl'
     depends_on 'jansson'
     depends_on 'libusb'
-  
+
     def install
+        inreplace "autogen.sh", "libtoolize", "glibtoolize" 
         inreplace "autogen.sh", "readlink", "greadlink"
+        system "autoreconf -fvi" 
         system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
         "--prefix=#{prefix}",
         "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig",
@@ -28,8 +30,8 @@ class Cgminer < Formula
         "--enable-avalon"
         system "make", "install"
     end
-  
+
     test do
-    system "cgminer"
-end
+        system "cgminer"
+    end
 end
