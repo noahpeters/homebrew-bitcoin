6390ec7cf3917b4917a80bd8df73ee0aae88c100
Vincent Gariepy <caktux@gmail.com>
2013-11-22 01:39:23 -0500

    Update cgminer to 3.7.2

--- cgminer.rb
+++ cgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Cgminer < Formula
     homepage 'https://github.com/ckolivas/cgminer'
     head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
-    url 'https://github.com/ckolivas/cgminer.git', :tag => 'v3.7.1'
-    version '3.7.1'
+    url 'https://github.com/ckolivas/cgminer.git', :tag => 'v3.7.2'
+    version '3.7.2'
 
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
