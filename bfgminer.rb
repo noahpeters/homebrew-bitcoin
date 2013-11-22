6c1e9dbd166980d1a00f21019929f58161de20a5
Vincent Gariepy <caktux@gmail.com>
2013-11-22 01:32:44 -0500

    Update bfgminer to 3.6.0

--- bfgminer.rb
+++ bfgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Bfgminer < Formula
     homepage 'https://github.com/luke-jr/bfgminer'
     head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
-    url 'https://github.com/luke-jr/bfgminer.git', :tag => 'bfgminer-3.5.2'
-    version '3.5.2'
+    url 'https://github.com/luke-jr/bfgminer.git', :tag => 'bfgminer-3.6.0'
+    version '3.6.0'
     
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
