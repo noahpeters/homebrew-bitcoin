6fdc5761befd9b1894a18e4ad6e917cdbf48ad91
Vincent Gariepy <caktux@gmail.com>
2013-11-22 01:39:53 -0500

    Update cgminer to 3.8.1

--- cgminer.rb
+++ cgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Cgminer < Formula
     homepage 'https://github.com/ckolivas/cgminer'
     head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
-    url 'https://github.com/ckolivas/cgminer.git', :tag => 'v3.8.0'
-    version '3.8.0'
+    url 'https://github.com/ckolivas/cgminer.git', :tag => 'v3.8.1'
+    version '3.8.1'
 
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
